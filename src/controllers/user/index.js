3faef422c310ae17c5fd2e9a11fad321e5bc7f6f
Olyazarichna <olya.zarichna@gmail.com>
2022-10-13 15:29:29 +0300

    updateVvatar

--- src/controllers/user/index.js
+++ src/controllers/user/index.js
@@ -3,11 +3,13 @@ const login = require("./login");
 const logout = require("./logout");
 const register = require("./register");
 const updateSubscription = require("./updateSubscription")
+const updateAvatar = require("./updateAvatar");
 
 module.exports = {
   getCurrent,
   login,
   logout,
   register,
-  updateSubscription
+  updateSubscription,
+  updateAvatar
 };
